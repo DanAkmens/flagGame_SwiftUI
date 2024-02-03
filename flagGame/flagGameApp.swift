5c09ab74c6447b6bd877198b5b3b55bbb1856260
Dan <Bdylan998@gmail.com>
2024-02-03 14:09:30 +0200

    Initial Commit

---
+++ flagGame/flagGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  flagGameApp.swift
+//  flagGame
+//
+//  Created by Dainis Putans on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct flagGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
