038f55b178296b849345d9d2b6cd10e9f4fbfadd
Dan <Bdylan998@gmail.com>
2024-02-05 14:50:55 +0200

    add 2 custom modifiers

--- flagGame/ContentView.swift
+++ flagGame/ContentView.swift
@@ -7,6 +7,25 @@
 
 import SwiftUI
 
+// used line 72
+struct FlagImage: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .clipShape(.rect(cornerRadius: 20))
+            .shadow(radius: 5)
+    }
+}
+// used line 76
+struct SpacingVStack: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 20)
+            .background(.regularMaterial)
+            .clipShape(.rect(cornerRadius: 20))
+    }
+}
+
 struct ContentView: View {
 
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
@@ -27,14 +46,14 @@ struct ContentView: View {
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
             ], center: .top, startRadius: 200, endRadius: 400)
                 .ignoresSafeArea()
-            
+
             VStack {
                 Spacer()
 
                 Text("Guess the Flag")
                     .font(.largeTitle.bold())
                     .foregroundStyle(.white)
-                
+
                 VStack(spacing: 15) {
                     VStack {
                         Text("Tap the flag of")
@@ -44,25 +63,21 @@ struct ContentView: View {
                         Text(countries[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
                     }
-                    
+
                     ForEach(0..<3) { number in
                         Button {
                             flagTapped(number)
                         } label: {
                             Image(countries[number])
-                                .clipShape(.rect(cornerRadius: 20))
-                                .shadow(radius: 5)
+                                .modifier(FlagImage())
                         }
                     }
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.vertical, 20)
-                .background(.regularMaterial)
-                .clipShape(.rect(cornerRadius: 20))
-                
+                .modifier(SpacingVStack())
+
                 Spacer()
                 Spacer()
-                
+
                 Text("Score: \(userScore)")
                     .foregroundStyle(.white)
                     .font(.title.bold())
